3b087ee1ffd6f9fd56acc1e78426afc1d4046a82
Piotr Klimczak <Piotr.Klimczak@ICEInsureTech.com>
2019-04-02 11:06:14 +0100

    FELIX-6095 Found better fix implementation for a problem after additional code analysis

--- connect/src/main/java/org/apache/felix/connect/PojoSRBundle.java
+++ connect/src/main/java/org/apache/felix/connect/PojoSRBundle.java
@@ -266,21 +266,7 @@ class PojoSRBundle implements Bundle, BundleRevisions
     @Override
     public URL getResource(String name)
     {
-        try
-        {
-            // We need to take resource from JAR bundle instead of classloader, as classloader is shared
-            URL result = new URL(getLocation() + name);
-            if (result.openConnection().getContent() != null) // Checking if requested resource really exists
-            {
-                return result;
-            }
-        }
-        catch (IOException e)
-        {
-            //no-op as requested resource does not exists
-        }
-        return null;
-
+        return m_revision.getEntry(name);
     }
 
     @Override
